5546575528c36c71d8fc46c3cd2096b998231141
Sumit Maharjan <sumit.maharjan10@gmail.com>
2025-01-21 19:40:23 +0545

    Animation on hover added to react icon

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -12,7 +12,7 @@ const Tech = () => {
 
       <div className='grid grid-cols-4 gap-y-10 my-8 justify-items-center'>
 
-        <DiReact size={150} />
+        <DiReact size={150} className='hover:fill-red-500 hover:scale-125 transition-all duration-300' />
         <DiJava size={150} />
         <FaVuejs size={150} />
         <FaCss3 size={150} />
