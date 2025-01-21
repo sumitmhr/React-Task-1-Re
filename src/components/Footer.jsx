14ddaa07ce4e4b4f0a3f54c222d9dc7ed8e3aacf
Sumit Maharjan <sumit.maharjan10@gmail.com>
2025-01-21 19:38:05 +0545

    Footer section finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-black w-full h-60 flex items-end justify-center py-16'
+    style={{
+      clipPath: 'polygon(0 40%, 100% 0, 100% 100%, 0 100%)'
+    }}
+    >
+      <footer className='text-white'>
+          <h1>Developed By</h1>
+          <h1>Someone Anonymous</h1>
+      </footer>
+    </div>
   )
 }
 
