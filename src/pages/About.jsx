7abca402e272d8972b2f9cce1ccb11ec93ea0e21
Sumit Maharjan <sumit.maharjan10@gmail.com>
2025-01-21 18:23:09 +0545

    Worked on About Nav

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h1>This is About</h1>
+    <div className='p-7'>
+      <h1 className='text-2xl mb-5'>This is About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eos alias nobis quis veritatis reprehenderit facere qui voluptas quam iusto! Ea dolor quisquam ullam ipsum ducimus cumque quidem amet! Error accusamus expedita tempore, voluptatum perspiciatis ipsam eius dolor eos doloribus qui voluptatem totam labore nulla! Ratione nisi molestias blanditiis, consequuntur totam exercitationem rem labore impedit vel expedita, delectus nemo hic sit magnam quae eligendi, fugit at. Accusantium alias similique obcaecati natus vel quaerat esse distinctio facilis voluptate. Nostrum, dolores! Dignissimos obcaecati quos quod architecto accusamus consequatur expedita consectetur magnam aliquid id explicabo suscipit doloremque quis, animi vitae culpa, modi laborum rem. Recusandae unde necessitatibus quisquam, provident saepe ullam, impedit ex, praesentium eius amet perferendis! Deleniti possimus iure repellendus cum accusantium, recusandae quaerat dolores illum dolor architecto eum? Delectus quam inventore beatae impedit et ipsum odit itaque ut non quas dicta neque fugiat nam ullam ad quis, iure expedita recusandae laudantium facere quia asperiores hic quidem! Labore delectus facere repellat repudiandae necessitatibus illo consequuntur. Fuga quibusdam, quasi, incidunt dolore accusamus vero harum praesentium ut sunt tempora aliquam, itaque ipsam. Voluptatem tenetur optio doloremque dicta doloribus provident nihil nobis? Esse consequuntur cumque voluptas rem, molestiae maxime animi quos assumenda dolorem expedita cupiditate veniam distinctio quae vero! Magnam ipsam consectetur hic quae, dolor voluptas corrupti molestias at eaque dicta ex rem distinctio molestiae ut commodi in qui possimus perspiciatis aperiam, incidunt odio. Deserunt, officiis. Officiis ea voluptatem ipsum molestiae id ipsam ratione dignissimos quasi explicabo blanditiis labore mollitia eum optio obcaecati eveniet distinctio itaque placeat repellat, maiores dolore ex dolor temporibus assumenda magni! Illum recusandae excepturi provident ex voluptatibus aut inventore officiis, labore eaque a commodi deserunt consequuntur neque cumque, ea repudiandae reiciendis ipsum natus, magni incidunt tempore nemo nesciunt fugiat? Laudantium, reprehenderit provident aperiam ab obcaecati debitis explicabo soluta. Iusto, maxime! Quidem?
+        </p>
     </div>
   )
 }
