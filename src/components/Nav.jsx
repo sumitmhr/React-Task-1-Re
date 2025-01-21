fc53c1dbac2a4202b73b929eb729911ab6d7f5d0
Sumit Maharjan <sumit.maharjan10@gmail.com>
2025-01-21 18:12:47 +0545

    CSS added for Nav Bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,14 +3,16 @@ import { NavLink } from 'react-router'
 
 const Nav = () => {
   return (
-    <div className='bg-black text-white px-4 py-2 flex justify-between'>
+    <div className='bg-black text-white px-4 py-2 flex justify-between sticky top-0 items-baseline'>
 
-      <a href='/' className='text-2xl'>Demo</a>
+      <a href='/'>
+        <h1 className='text-2xl'>Demo</h1>
+      </a>
 
-      <nav>
+      <nav className='flex gap-3'>
 
-        <NavLink to='/about'>About</NavLink>
-        <NavLink to='/contact'>Contact</NavLink>
+        <NavLink className='hover:bg-white hover:text-black p-2' to='/about'>About</NavLink>
+        <NavLink className='hover:bg-white hover:text-black p-2' to='/contact'>Contact</NavLink>
 
       </nav>
       
