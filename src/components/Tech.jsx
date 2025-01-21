123c8a16aa56e57e460369a05f97ad7b29d5db9a
Sumit Maharjan <sumit.maharjan10@gmail.com>
2025-01-21 19:17:34 +0545

    npm i react-icons done and Tech section finished

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { DiJava, DiPython, DiReact } from 'react-icons/di'
+import { FaCss3, FaSwift, FaVuejs } from "react-icons/fa"
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div>
+      
+      <h1 className='text-4xl text-center font-bold'>
+        Technologies I Use
+      </h1>
+
+      <div className='grid grid-cols-4 gap-y-10 my-8 justify-items-center'>
+
+        <DiReact size={150} />
+        <DiJava size={150} />
+        <FaVuejs size={150} />
+        <FaCss3 size={150} />
+        <DiPython size={150} />
+        <FaSwift size={150} />
+
+      </div>
+
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import DisplaySection from '../components/DisplaySection'
+import Tech from '../components/Tech'
 
 const Home = () => {
   return (
     <div>
 
       <DisplaySection />
+
+      <Tech />
 
     </div>
   )
